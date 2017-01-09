7640670e8c2f7480409d3bb9fa372a8616f1feb5
Denis <denis.o@linux.com>
2017-01-09 17:45:10 +0200

    Added jMinix dependency, jMinix servlet registration

--- src/main/java/com/unidev/eurekaregistry/EurekaApplication.java
+++ src/main/java/com/unidev/eurekaregistry/EurekaApplication.java
@@ -1,14 +1,24 @@
 package com.unidev.eurekaregistry;
 
+import org.jminix.console.servlet.MiniConsoleServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaApplication implements ServletContextInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.addServlet("JmxMiniConsoleServlet", MiniConsoleServlet.class).addMapping("/jmx/*");
 	}
 }
